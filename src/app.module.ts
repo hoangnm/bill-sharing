e5aa89b345547c71309a859ae4375e4ab296f4e6
hoangnm <nguyenminhhoang210891@gmail.com>
2022-02-28 01:00:19 +0700

    get config from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { TripsModule } from './modules/trips/trips.module';
+
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://localhost/bill-sharing?authSource=admin',
-      {
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: 'mongodb://localhost/bill-sharing?authSource=admin',
         auth: {
-          username: 'hoangnm',
-          password: '123456',
+          username: config.get('DB_USER'),
+          password: config.get('DB_PASS'),
         },
         autoCreate: true,
-      },
-    ),
+      }),
+    }),
+    ConfigModule.forRoot(),
     TripsModule,
   ],
 })
