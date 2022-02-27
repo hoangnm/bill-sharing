e3828ffddbda1bcfb07812c97063042186dc4155
hoangnm <nguyenminhhoang210891@gmail.com>
2022-02-28 00:36:01 +0700

    add class-validator

--- src/modules/trips/dto/create-bill.dto.ts
+++ src/modules/trips/dto/create-bill.dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
 export class CreateBillDto {
+  @IsNotEmpty()
   description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   amount: number;
+
+  @IsNotEmpty()
   participants: string[];
+
+  @IsNotEmpty()
   tripId: string;
 }
